d040e3c40d93de20d67ed23bb400aafa5a39c3d4
Marcel <112168700+marcelkoxpvp2137@users.noreply.github.com>
2023-09-11 13:44:32 +0200

    Add files via upload

--- Marcel Kaliński 2p/1.cpp
+++ Marcel Kaliński 2p/1.cpp
@@ -1,64 +1,49 @@
 #include <iostream>
-#include <cmath>
+#include <string>
+#include <math.h>
 using namespace std;
-int ax, ay;
-int bx, by;
-int cx, cy;
-float dab, dac, dbc;
-float funkcja(int x1,int y1, int x2, int y2)
+
+string liczba;
+int y=0, wynik=0;
+
+int pierwsza(int wynik)
 {
-    return sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+    if(wynik<2)
+    {
+        cout<<"Liczba nie jest pierwsza";
+        return 0;
+    }
+    for (int i = 2; i < wynik; i++)
+    {
+    if (wynik % i == 0)
+    {
+        cout<<"Liczba nie jest pierwsza";
+        return 0;
+    }
+
+    }
+    cout<<"Liczba jest pierwsza";
+    return 0;
+
 }
-int main()
+int main ()
 {
-cout<<"Podaj wspolrzedne punktu AX ";
-cin>>ax;
-while(ax>9 || ax<0)
-{
-    cout<<"Podaj liczbe z przedzialu <0,9>"<<endl;
-    cin>>ax;
-}
-cout<<"Podaj wspolrzedne punktu AY ";
-cin>>ay;
-while(ay>9 || ay<0)
-{
-    cout<<"Podaj liczbe z przedzialu <0,9>"<<endl;
-    cin>>ay;
-}
-cout<<"Podaj wspolrzedne punktu BX ";
-cin>>bx;
-while(bx>9 || bx<0)
-{
-    cout<<"Podaj liczbe z przedzialu <0,9>"<<endl;
-    cin>>bx;
-}
-cout<<"Podaj wspolrzedne punktu BY ";
-cin>>by;
-while(by>9 || by<0)
-{
-    cout<<"Podaj liczbe z przedzialu <0,9>"<<endl;
-    cin>>by;
-}
-cout<<"Podaj wspolrzedne punktu CX ";
-cin>>cx;
-while(cx>9 || cx<0)
-{
-    cout<<"Podaj liczbe z przedzialu <0,9>"<<endl;
-    cin>>cx;
-}
-cout<<"Podaj wspolrzedne punktu CY ";
-cin>>cy;
-while(cy>9 || cy<0)
-{
-    cout<<"Podaj liczbe z przedzialu <0,9>"<<endl;
-    cin>>cy;
+    do
+    {
+        cout<<"Podaj liczbe maksymalnie 6 cyfrowa"<<endl;
+        cin>>liczba;
+    }
+    while(liczba.length()>6);
+
+y = stoi(liczba);
+
+    do
+    {
+        wynik=wynik+y%10;
+        y=y/10;
+    }
+    while(y!=0);
+
+    cout<<pierwsza(wynik);
 }
 
-dab=funkcja(ax,ay,bx,by);
-dbc=funkcja(bx,by,cx,cy);
-dac=funkcja(ax,ay,cx,cy);
-
-cout<<"Odleglosc AB wynosi "<<dab<<endl;
-cout<<"Odleglosc BC wynosi "<<dbc<<endl;
-cout<<"Odleglosc AC wynosi "<<dac<<endl;
-}
